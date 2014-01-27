306b0fe018caa004b8293c99cf445ff2c76bfcc7
Andrew Shim <andrewkshim92@gmail.com>
2014-01-27 14:46:51 -0500

    setup.py for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'SurveyMonty',
+    packages = ['SurveyMonty'], # this must be the same as the name above
+    version = '1.0',
+    description = 'Python wrapper for SurveyMonkey API',
+    author = 'Andrew Shim',
+    author_email = 'andrewkshim92@gmail.com',
+    url = 'https://github.com/andrewkshim/SurveyMonty',   # use the URL to the github repo
+    download_url = 'https://github.com/andrewkshim/SurveyMonty/archive/master.zip', # I'll explain this in a second
+    keywords = ['SurveyMonkey'],
+    classifiers = [],
+)
