dd13792768e68aef01ca55546819b15727eb76eb
Andrew Shim <andrewkshim92@gmail.com>
2014-02-11 18:05:52 -0500

    added license to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
-    name = 'surveymonty',
-    py_modules = ['surveymonty'], # this must be the same as the name above
-    version = '0.1.9',
-    description = 'Python wrapper for SurveyMonkey API',
-    author = 'Andrew Shim',
-    author_email = 'andrewkshim92@gmail.com',
-    url = 'https://github.com/andrewkshim/SurveyMonty',   # use the URL to the github repo
-    download_url = 'https://github.com/andrewkshim/SurveyMonty/tarball/1.0', # I'll explain this in a second
-    keywords = ['SurveyMonkey'],
-    classifiers = [],
+    name='surveymonty',
+    py_modules=['surveymonty'],
+    version='0.1.9',
+    description='Python wrapper for SurveyMonkey API',
+    author='Andrew Shim',
+    author_email='andrewkshim92@gmail.com',
+    url='https://github.com/andrewkshim/SurveyMonty',
+    download_url='https://github.com/andrewkshim/SurveyMonty/tarball/1.0',
+    keywords=['SurveyMonkey'],
+    license='MIT',
+    classifiers=[],
 )
